54b667ce3f91ca7c62e76e03d72655ed20f71472
Bryan Cai <bxcai@stanford.edu>
2023-12-27 19:50:16 -0500

    Initialization

---
+++ CELEHS_LLM_Scripts/README.md
@@ -0,0 +1,194 @@
+# Training and Inference of Large Language Models (LLMs) for Clinical Purposes
+
+## The Model List:
+
+For LLMs:
+   1. [meta-llama/Llama-2-7b-chat-hf](https://huggingface.co/meta-llama/Llama-2-7b-chat-hf)
+   2. [WizardLM/WizardLM-7B-V1.0](https://huggingface.co/WizardLM/WizardLM-7B-V1.0)
+   3. [lmsys/vicuna-7b-v1.5](https://huggingface.co/lmsys/vicuna-7b-v1.5)
+   4. [lmsys/vic7b-una-v1.5-16k](https://huggingface.co/lmsys/vicuna-7b-v1.5-16k)
+
+For SentenceBERT:
+   1. [sentence-transformers/all-mpnet-base-v2](https://huggingface.co/sentence-transformers/all-mpnet-base-v2)
+   2. [sentence-transformers/multi-qa-mpnet-base-dot-v1](https://huggingface.co/sentence-transformers/multi-qa-mpnet-base-dot-v1)
+   3. [BAAI/bge-base-en-v1.5](https://huggingface.co/BAAI/bge-base-en-v1.5) ## IF using bge, activate normalized embeddings in sentencebert: model.encode(sentences_1, normalize_embeddings=True)
+   4. [BAAI/bge-large-en-v1.5](https://huggingface.co/BAAI/bge-large-en-v1.5)
+
+For ColBERT:
+   The checkpoint can be downloaded from [here](https://downloads.cs.stanford.edu/nlp/data/colbert/colbertv2/colbertv2.0.tar.gz).
+
+## Inference
+
+To start with, current open-source LLMs mainly based on PyTorch and HuggingFace Hubs. The first step is to set up environments:
+
+1. Download Anaconda for better management of virtual environments. To download and install, using the following line in the terminal:
+   ```{bash}
+   cd <where-to-download>
+   wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+   bash Anaconda3-2023.09-0-Linux-x86_64.sh
+   ```
+2. After installation, we can create an virtual environment by:
+   ```{bash}
+   conda create -n inference python=3.10
+   ```
+   then we can go into this environment by:
+   ```{bash}
+   conda activate inference
+   ```
+3. We can set the python environment with pip:
+   ```{bash}
+   pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117
+   pip install -r requirements.txt
+   pip install "fschat[model_worker,webui]"
+   pip install -U sentence-transformers
+   pip install -U FlagEmbedding
+   ```
+
+<!-- For runing inference, we have to prepare two things locally:
+1. Local LLM Weights, if the nodes can have access to the internet, we can also use the online HuggingFace Model Hubs.
+2. Local File for inference, to fit this code, the file have to be reformatted to a .jsonl file, in which each line presents one sample and is formatted as:
+   ```
+   {'sample_idx': <sample_idx>, 'instruction': <input_query>}
+   ```
+   Basically, we can use json package in Python to generate such formats. We have an code example [here](./inference/generate_dummydata.py).
+
+
+Here can we start inference! Using these lines:
+```{bash}
+cd <path-to-this-repo>/inference/
+bash ./llm_inference.sh <local_model_path> <input_jsonl_file> <output_file_directory_name> <number-of-gpu-to-use>
+```
+Then we can check the output jsonl file in output/<output_file_directory_name>/results.jsonl.
+ -->
+
+
+<!-- ## Fine-tuning
+
+
+
+To start with, current open-source LLMs mainly based on PyTorch and HuggingFace Hubs. The first step is to set up environments:
+
+1. Download Anaconda for better management of virtual environments. To download and install, using the following line in the terminal:
+   ```{bash}
+   cd <where-to-download>
+   wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+   bash Anaconda3-2023.09-0-Linux-x86_64.sh
+   ```
+2. After installation, we can create an virtual environment by:
+   ```{bash}
+   conda create -n llm_inference python=3.10
+   ```
+   then we can go into this environment by:
+   ```{bash}
+   conda activate llm_finetune
+   ```
+3. We can set the python environment with pip:
+
+   *Note: Below is the instruction for A100 for Vidul*
+   ```{bash}
+   conda install cudatoolkit-dev -c conda-forge
+   conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 -c pytorch -c nvidia
+   cd <path-to-this-repo>/fine-tune/
+   pip install -r requirements.txt
+   ```
+   Then we have to install flash-attention for accelerated training, [flash-attn](https://github.com/Dao-AILab/flash-attention): 
+   To install:
+   1. Make sure that PyTorch is installed.
+   2. Make sure that `packaging` is installed (`pip install packaging`)
+   3. Make sure that `ninja` is installed and that it works correctly (e.g. `ninja
+   --version` then `echo $?` should return exit code 0). If not (sometimes `ninja
+   --version` then `echo $?` returns a nonzero exit code), uninstall then reinstall
+   `ninja` (`pip uninstall -y ninja && pip install ninja`). Without `ninja`,
+   compiling can take a very long time (2h) since it does not use multiple CPU
+   cores. With `ninja` compiling takes 3-5 minutes on a 64-core machine.
+   4. Then:
+   ```sh
+   pip install flash-attn --no-build-isolation
+   ```
+   Alternatively you can compile from source:
+   ```sh
+   python setup.py install
+   ```
+
+
+   *Note: Below is the instruction for V100 for Sara*
+   ```{bash}
+   conda install cudatoolkit-dev -c conda-forge
+   conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 -c pytorch -c nvidia
+   cd <path-to-this-repo>/fine-tune/
+   pip install -r requirements.txt
+   ```
+   
+
+
+
+After setting up environments, for runing fine-tuning, we also have to prepare two things locally:
+1. Local LLM Weights, if the nodes can have access to the internet, we can also use the online HuggingFace Model Hubs.
+2. Local File for fine-tuning, to fit this code, the file have to be reformatted to a .jsonl file, in which each line presents one sample and is formatted as:
+   ```
+   {'sample_idx': <sample_idx>, 'instruction': <input_query>, 'output': <input_query>}
+   ```
+   Basically, we can use json package in Python to generate such formats. We have an code example [here](./inference/generate_dummydata.py).
+
+*It should be noted that: Flash Attention which is a framework for fast fine-tuning with higher speed and low memory cost does not support V100. Therefore for A100, you can use llama_train.sh or llama2_train.sh for fine-tuning, which depends on the LLM you base on. For V100, you can use main_llama_noflash.sh for both llama and llama2. And This is why we have different env set up for Vidul and Sara.* 
+
+Here can we start fine-tuning! Using these lines:
+```{bash}
+cd <path-to-this-repo>/fine-tune/
+bash ./llama_train.sh <local_model_path> <input_jsonl_file> <output_file_directory_name> <number-of-gpu-to-use>
+```
+Then we can check the fine-tuned model in output/<output_file_directory_name>/
+
+
+
+
+## SentenceBERT
+
+Setting up environments:
+
+1. Download Anaconda for better management of virtual environments. To download and install, using the following line in the terminal:
+   ```{bash}
+   cd <where-to-download>
+   wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+   bash Anaconda3-2023.09-0-Linux-x86_64.sh
+   ```
+2. After installation, we can create an virtual environment by:
+   ```{bash}
+   conda create -n sentencebert python=3.10
+   ```
+   then we can go into this environment by:
+   ```{bash}
+   conda activate sentencebert
+   ```
+3. We can set the python environment with pip:
+   ```{bash}
+   pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117
+   cd <path-to-this-repo>/sentenceBERT/
+   pip install -r requirements.txt
+   pip install -U sentence-transformers
+   ```
+
+After Setting Up environment, we have to download the embedding model locally or use the Huggingface Hub. The accessible models are listed [here](https://www.sbert.net/docs/pretrained_models.html#). 
+
+Then prepare all your sentences or terms for embeddings to a jsonl file with each line, here is an example:
+```
+{'input': 'harvard medical school'}
+```
+
+Finally, we can generate the sentence embedding by:
+```{bash}
+cd <path-to-this-repo>/sentenceBERT/
+CUDA_VISIBLE_DEVICES=0 python main.py <jsonl-file-of-sentences> <local-model-path> <output-dir-name>
+```
+The sentence embeddings will be saved to a .pkl file in './sentenceBERT/output/output-dir-name/embeddings.pkl'
+
+To load the saved sentence embedding for other use, you can run the following lines in a Python:
+```{python}
+import pickle
+with open(<path-to-embeddings.pkl>, "rb") as fIn:
+    stored_data = pickle.load(fIn)
+    stored_sentences = stored_data['sentences']
+    stored_embeddings = stored_data['embeddings']
+```
+ -->
+
---
+++ CELEHS_LLM_Scripts/inference/inference.py
@@ -0,0 +1,254 @@
+import argparse
+import json
+import os
+import torch
+from tqdm import tqdm
+from vllm import LLM, SamplingParams
+from fastchat.model.model_adapter import model_adapters, register_model_adapter, BaseModelAdapter
+from fastchat.conversation import conv_templates, register_conv_template, Conversation, SeparatorStyle, get_conv_template
+
+few_shot_question_template = 'Given the contexts: {context}, please answer: {question} '
+with open('./few_shot_example.jsonl', 'r') as f:
+    incontext_few_shots = [json.loads(item) for item in f.readlines()]
+messages = [[("Human", few_shot_question_template.format(context=item['context'], question=item["question"])), ("Assistant", item["answer"])] for item in incontext_few_shots]
+messages = tuple(sum(messages, []))
+
+register_conv_template(
+    Conversation(
+        name="custom_few_shot",
+        system_message="A chat between a curious human and an artificial intelligence clinician. "
+        "The assistant gives helpful and detailed answers to the human's questions related to biomedicine.",
+        roles=("Human", "Assistant"),
+        messages=messages,
+        offset=2,
+        sep_style=SeparatorStyle.ADD_COLON_SINGLE,
+        sep="\n### ",
+        stop_str="###",
+    )
+)
+
+register_conv_template(
+    Conversation(
+        name="pretrain_few_shot",
+        system_message="",
+        roles=("Question", "Answer"),
+        messages=messages,
+        offset=2,
+        sep_style=SeparatorStyle.ADD_COLON_SINGLE,
+        sep="\n### ",
+        stop_str="###",
+    )
+)
+
+register_conv_template(
+    Conversation(
+        name="one_shot",
+        system_message="A chat between a curious human and an artificial intelligence clinician. "
+        "The assistant gives helpful and detailed answers to the human's questions related to biomedicine.",
+        roles=("Human", "Assistant"),
+        messages=messages[:2],
+        offset=2,
+        sep_style=SeparatorStyle.ADD_COLON_SINGLE,
+        sep="\n### ",
+        stop_str="###",
+    ),
+    override=True
+)
+
+register_conv_template(
+    Conversation(
+        name="eevee",
+        system_message="Below is a health record paired with a question that describes a task.  Write a response that appropriately answers the question.",
+        roles=("### Health Record", "### Answer"),
+        sep_style=SeparatorStyle.ADD_COLON_TWO,
+        sep="\n\n",
+        sep2="</s>",
+    )
+)
+
+class EeveeAdapter(BaseModelAdapter):
+
+    use_fast_tokenizer = False
+
+    def match(self, model_path: str):
+        return "eevee" in model_path.lower()
+
+    def get_default_conv_template(self, model_path: str):
+        return get_conv_template("eevee")
+
+class FewShotAdapter(BaseModelAdapter):
+
+    use_fast_tokenizer = False
+
+    def match(self, model_path: str):
+        return "fewshot" in model_path.lower() and "pretrain" not in model_path.lower()
+
+    def get_default_conv_template(self, model_path: str):
+        return get_conv_template("custom_few_shot")
+
+class PretrainFewShotAdapter(BaseModelAdapter):
+
+    use_fast_tokenizer = False
+
+    def match(self, model_path: str):
+        return "pretrainfewshot" in model_path.lower()
+
+    def get_default_conv_template(self, model_path: str):
+        return get_conv_template("pretrain_few_shot")
+
+register_model_adapter(EeveeAdapter)
+register_model_adapter(FewShotAdapter)
+register_model_adapter(PretrainFewShotAdapter)
+
+from fastchat.model import get_conversation_template
+import numpy as numpy
+from transformers import AutoTokenizer
+
+def run_eval(
+    model_path,
+    model_id,
+    questions,
+    answer_file,
+    max_new_token,
+    tp_size,
+):
+    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
+    special_tokens_dict = dict()
+    if tokenizer.pad_token is None:
+        special_tokens_dict["pad_token"] = '<pad>'
+    if tokenizer.eos_token is None:
+        special_tokens_dict["eos_token"] = '</s>'
+    if tokenizer.bos_token is None:
+        special_tokens_dict["bos_token"] = '<s>'
+    if tokenizer.unk_token is None:
+        special_tokens_dict["unk_token"] = '<unk>'
+    if len(special_tokens_dict) > 0:
+        tokenizer.add_special_tokens(special_tokens_dict)
+        tokenizer.save_pretrained(model_path)
+    try:
+        model = LLM(model=model_path, tensor_parallel_size=tp_size)
+    except RecursionError:
+        model = LLM(model=model_path, tokenizer_mode='slow', tensor_parallel_size=tp_size)
+    print('model loadeds')
+    sampling_params = SamplingParams(temperature=0.7, max_tokens=max_new_token)
+
+    prompts = []
+
+
+    # if conv.name=='eevee':
+    #     question_template = '{context}\n\n### Question:\n{question}'
+    # else:
+    #     question_template = few_shot_question_template
+        
+    for item in tqdm(questions):
+        torch.manual_seed(0)
+        if 'llama' in model_id.lower() and 'chat' not in model_id.lower():
+            conv = get_conversation_template('pretrainfewshot')
+        else:
+            conv = get_conversation_template(model_id)
+        qs = few_shot_question_template.format(context=item['context'], question=item["question"])
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+        prompts.append(prompt)
+
+    prompt_id_map = {prompt: idx for idx, prompt in enumerate(prompts)}
+
+    outputs = model.generate(prompts, sampling_params)
+
+    for output in outputs:
+        output_ids = output.outputs[0].token_ids
+        question = questions[prompt_id_map[output.prompt]]
+
+        # be consistent with the template's stop_token_ids
+        if conv.stop_token_ids:
+            stop_token_ids_index = [
+                i
+                for i, id in enumerate(output_ids)
+                if id in conv.stop_token_ids
+            ]
+            if len(stop_token_ids_index) > 0:
+                output_ids = output_ids[: stop_token_ids_index[0]]
+
+        output = model.get_tokenizer().decode(
+            output_ids,
+            spaces_between_special_tokens=False,
+        )
+        if conv.stop_str and output.find(conv.stop_str) > 0:
+            output = output[: output.find(conv.stop_str)]
+        for special_token in model.get_tokenizer().special_tokens_map.values():
+            if isinstance(special_token, list):
+                for special_tok in special_token:
+                    output = output.replace(special_tok, "")
+            else:
+                output = output.replace(special_token, "")
+        output = output.strip()
+
+
+        question['output'] = output
+        question['generator'] = model_id
+
+        # Dump answers
+        os.makedirs(os.path.dirname(answer_file), exist_ok=True)
+        with open(os.path.expanduser(answer_file), "a") as fout:
+            fout.write(json.dumps(question) + "\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-path",
+        type=str,
+        required=True,
+        help="The path to the weights. This can be a local folder or a Hugging Face repo ID.",
+    )
+    parser.add_argument("--model-id", type=str, required=True)
+    parser.add_argument(
+        "--question-file",
+        type=str,
+        default=None,
+        help="The name of the benchmark question set.",
+    )
+    parser.add_argument(
+        "--answer-file",
+        type=str,
+        default=None,
+        help="The output answer file.",
+    )
+    parser.add_argument(
+        "--max-new-token",
+        type=int,
+        default=1024,
+        help="The maximum number of new generated tokens.",
+    )
+
+    args = parser.parse_args()
+
+    with open(args.question_file, 'r') as f:
+        questions = [json.loads(item) for item in f.readlines()]
+    
+    if 'WORLD_SIZE' in os.environ and int(os.environ['WORLD_SIZE']) > 1:
+        num_replicas = int(os.environ['WORLD_SIZE'])
+        rank = int(os.environ['RANK'])
+        tp_size = torch.cuda.device_count() // num_replicas
+        devices = ','.join([str(i) for i in range(rank*tp_size, (rank+1)*tp_size)])
+        # torch.cuda.set_device(devices)
+        total_size = len(questions)
+        questions = questions[rank:total_size:num_replicas]
+        args.answer_file = args.answer_file.replace(".jsonl", f"_{rank}.jsonl")
+
+        print(f"RANK: {rank} | NUM_REPLICAS: {num_replicas} | devices : {devices}")
+    else:
+        tp_size = 1
+
+    print(f"Output to {args.answer_file}")
+    print(f"Num Questions: {len(questions)}")
+    print(f"Conv Template: {get_conversation_template(args.model_id)}")
+    
+    run_eval(
+        args.model_path,
+        args.model_id,
+        questions,
+        args.answer_file,
+        args.max_new_token,
+        tp_size,
+    )
